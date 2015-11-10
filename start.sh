bc18ab80f08d902a5ce00f18bac7953fb7046e87
Michael Johnson <michael@johnson.computer>
2015-11-10 18:02:29 -0500

    Remove extra newline

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 
 if [ ! -e "/config/nzbget.conf" ]; then
 	gunzip -c /usr/share/doc/nzbget/examples/nzbget.conf.gz | sed 's/^MainDir=.*$/MainDir=\/downloads/g' > /config/nzbget.conf
-
 fi
 
 exec /usr/bin/nzbget -D -c /config/nzbget.conf

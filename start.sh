4afc4877ba34f8899b9b71b0a3addc2c43cff803
Michael Johnson <michael@johnson.computer>
2015-11-11 09:32:43 -0500

    Change default configuration location

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -e "/config/nzbget.conf" ]; then
-	gunzip -c /usr/share/doc/nzbget/examples/nzbget.conf.gz | sed 's/^MainDir=.*$/MainDir=\/downloads/g' > /config/nzbget.conf
+	cat /usr/share/nzbget/nzbget.conf | sed 's/^MainDir=.*$/MainDir=\/downloads/g' > /config/nzbget.conf
 fi
 
 exec /usr/bin/nzbget -s -c /config/nzbget.conf -o outputmode=log

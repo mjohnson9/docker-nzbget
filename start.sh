b7588b2e2a93999fef97955105e3374ca4bef180
Michael Johnson <michael@johnson.computer>
2015-11-11 09:18:16 -0500

    Don't daemonize on start

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ if [ ! -e "/config/nzbget.conf" ]; then
 	gunzip -c /usr/share/doc/nzbget/examples/nzbget.conf.gz | sed 's/^MainDir=.*$/MainDir=\/downloads/g' > /config/nzbget.conf
 fi
 
-exec /usr/bin/nzbget -D -c /config/nzbget.conf -o outputmode=color
+exec /usr/bin/nzbget -s -c /config/nzbget.conf -o outputmode=color
